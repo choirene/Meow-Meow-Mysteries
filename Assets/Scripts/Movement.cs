cba2eab3cef968e9aaa9a7660f85553f79d84985
Irene <choireneh@gmail.com>
2023-01-26 18:22:36 -0500

    Added animation to percy and assets

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public float speed;
+    public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,32 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, vertical);
+
+        AnimateMovement(direction);
+
+        transform.position += direction * speed * Time.deltaTime;
+
+    }
+
+    void AnimateMovement(Vector3 direction)
+    {
+        if(animator != null)
+        {
+            if(direction.magnitude > 0)
+            {
+                animator.SetBool("isMoving", true);
+
+                animator.SetFloat("horizontal", direction.x);
+                animator.SetFloat("vertical", direction.y);
+            }
+            else
+            {
+                animator.SetBool("isMoving", false);
+            }
+        }
     }
 }
