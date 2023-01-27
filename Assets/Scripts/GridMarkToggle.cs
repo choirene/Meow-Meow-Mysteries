db84da63ad9b1689d91d1b57bb890ebea00a3b6e
Irene <choireneh@gmail.com>
2023-01-27 14:54:25 -0500

    Added full functionality to grid UI

---
+++ Assets/Scripts/GridMarkToggle.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMarkToggle : MonoBehaviour
+{
+    // public GameObject wrong;
+    // public GameObject right;
+    [SerializeField] public List<GameObject> markSprites;
+    public int currentMark;
+
+    public void ToggleMark()
+    {
+        currentMark ++;
+        currentMark = currentMark % 3;
+
+        if(currentMark == 0)
+        {
+            markSprites[2].SetActive(false);
+        }
+
+        for(int i = 1; i < 3; i++)
+        {
+            if(i == currentMark)
+            {
+                markSprites[i-1].SetActive(false);
+                markSprites[i].SetActive(true);
+            }
+        }
+    }
+    void Start()
+    {
+        currentMark = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
