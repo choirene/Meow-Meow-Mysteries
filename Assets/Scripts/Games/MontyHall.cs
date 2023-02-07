36f4d75e2d167a35dc670b0853921a5a9698bff3
Irene <choireneh@gmail.com>
2023-02-07 17:15:16 -0500

    finished monty hall mini game

--- Assets/Scripts/Games/MontyHall.cs
+++ Assets/Scripts/Games/MontyHall.cs
@@ -1,14 +1,119 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class MontyHall : MonoBehaviour
 {
-    [SerializeField] Sprite[] catSprites;
-    [SerializeField] GameObject[] closedDoors;
+    [SerializeField] Image[] catGameObjects;
+    [SerializeField] Image[] closedDoors;
+
+    [Header("Buttons")]
+    [SerializeField] GameObject switchButton;
+    [SerializeField] GameObject keepButton;
+    [SerializeField] GameObject winButton;
+    [SerializeField] GameObject loseButton;
+    [SerializeField] GameObject playAgainButton;
+    [Header("Text")]
+    [SerializeField] GameObject beginningText;
+    [SerializeField] GameObject switchText;
+    [SerializeField] GameObject switchDoorsText;
+    [SerializeField] GameObject endText;
+    private int cocoIndex;
+    private bool choiceActive;
+    private int doorChoiceIndex;
+    private bool secondChoice;
 
     public void StartGame()
     {
-        Debug.Log("started");
+        switchButton.SetActive(false);
+        keepButton.SetActive(false);
+        winButton.SetActive(false);
+        loseButton.SetActive(false);
+        playAgainButton.SetActive(false);
+        beginningText.SetActive(true);
+        switchText.SetActive(false);
+        switchDoorsText.SetActive(false);
+        endText.SetActive(false);
+        for(int i = 0; i < 5; i++)
+        {
+            catGameObjects[i].enabled = false;
+            closedDoors[i].enabled = true;
+        }
+        int index = Random.Range(0,5);
+        cocoIndex = index;
+        catGameObjects[index].enabled = true;
+
+        choiceActive = true;
+        secondChoice = false;
+    }
+
+    public void ChooseDoor(int index)
+    {
+        if(choiceActive)
+        {
+            doorChoiceIndex = index;
+        }
+
+        if(secondChoice)
+        {
+            EndGame();
+        }
+        else
+        {
+            AskSwitchDoor();
+        }
+    }
+
+    void AskSwitchDoor()
+    {
+        choiceActive =false;
+        int randomIndex = Random.Range(0,5);
+        while(randomIndex == cocoIndex || randomIndex == doorChoiceIndex)
+        {
+            randomIndex = Random.Range(0,5);
+        }
+        closedDoors[randomIndex].enabled = false;
+        beginningText.SetActive(false);
+        switchText.SetActive(true);
+        switchButton.SetActive(true);
+        keepButton.SetActive(true);
+    }
+
+    public void SwitchChoice()
+    {
+        switchText.SetActive(false);
+        switchButton.SetActive(false);
+        keepButton.SetActive(false);
+        switchDoorsText.SetActive(true);
+        choiceActive = true;
+        secondChoice = true;
+    }
+
+    public void EndGame()
+    {
+        choiceActive = false;
+        switchText.SetActive(false);
+        switchButton.SetActive(false);
+        keepButton.SetActive(false);
+        switchDoorsText.SetActive(false);
+        endText.SetActive(true);
+
+        foreach(var door in closedDoors)
+        {
+            door.enabled = false;
+        }
+        if(doorChoiceIndex == cocoIndex)
+        {
+            endText.GetComponent<TMP_Text>().text = "You found her!";
+            winButton.SetActive(true);
+        }
+        else
+        {
+            endText.GetComponent<TMP_Text>().text = "She got away...";
+            playAgainButton.SetActive(true);
+            loseButton.SetActive(true);
+        }
     }
 }
