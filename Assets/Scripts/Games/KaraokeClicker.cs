d2d89c7cf26927808615fbb578296582244025a8
Irene <choireneh@gmail.com>
2023-02-06 23:12:40 -0500

    added basic ui layout for karaoke game and started boxing out variables in script

--- Assets/Scripts/Games/KaraokeClicker.cs
+++ Assets/Scripts/Games/KaraokeClicker.cs
@@ -1,11 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KaraokeClicker : MonoBehaviour
 {
+    [SerializeField] Slider basilSlider;
+    [SerializeField] Slider percySlider;
+    float timeLimit = 10f;
+    float timerValue;
     public void StartGame()
     {
-        Debug.Log("Start game");
+        basilSlider.value = 0;
+        percySlider.value = 0;
+    }
+
+    public void ClickCat()
+    {
+        if(percySlider.value <= 100)
+        {
+            percySlider.value ++;
+        }
     }
 }
