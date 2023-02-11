466bea85f8a51086f616538a7947b4d279f1e432
Irene <choireneh@gmail.com>
2023-02-11 18:57:20 -0500

    added flavor text to all furniture

--- Assets/Scripts/Items and Inventory/FurnitureSO.cs
+++ Assets/Scripts/Items and Inventory/FurnitureSO.cs
@@ -7,7 +7,6 @@ using UnityEngine;
 public class FurnitureSO : ScriptableObject
 {
     [TextArea(3,5)] [SerializeField] public string flavorText;
-    [SerializeField] public int id;
     [SerializeField] public ItemSO[] potentialYields;
     [SerializeField] public bool yieldPossible;
 }
